49343c6d721e06f2838cc34cec5bd7d56e278c3a
John Pino <iam@johnpino.me>
2020-02-25 01:02:21 -0500

    Refactor toggle icon.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,18 @@
 <template>
   <header class="header">
+    <div class="header__toggle" v-on:click="toggleContent"><i class="fas fa-bars"></i></div>
     <h1 class="header__title"><i class="fad fa-ticket-alt"></i> The Awesome Movie App <i class="fad fa-ticket-alt"></i></h1>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  methods: {
+    toggleContent () {
+      console.log('click')
+    }
+  }
 }
 </script>
 
