2912769cb3a0be9874ffca760ee8911c7de5ce56
John Pino <iam@johnpino.me>
2020-02-24 23:27:07 -0500

    Add Fontawesome support

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,11 @@ import Header from '@/components/Header'
 import Sidebar from '@/components/Sidebar'
 import MainContent from '@/components/MainContent'
 export default {
-  components: { MainContent, Sidebar, Header }
+  components: { MainContent, Sidebar, Header },
+  mounted () {
+    const fontAwesome = document.createElement('script')
+    fontAwesome.setAttribute('src', 'https://kit.fontawesome.com/03888eec40.js')
+    document.head.appendChild(fontAwesome)
+  }
 }
 </script>
