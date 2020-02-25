9c4e4c294e5b9fa17c34220142d73526f02f370e
John Pino <iam@johnpino.me>
2020-02-24 23:33:49 -0500

    Improve Sidebar component.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,8 +1,8 @@
 <template>
   <aside class="sidebar">
-    <router-link to="/" class="sidebar__link">Home</router-link>
-    <router-link to="/top-10" class="sidebar__link">Top 10</router-link>
-    <router-link to="/latest" class="sidebar__link">Latest</router-link>
+    <router-link to="/" class="sidebar__link"><i class="fas fa-home"></i> Home</router-link>
+    <router-link to="/top-10" class="sidebar__link"><i class="fas fa-star"></i> Top 10</router-link>
+    <router-link to="/latest" class="sidebar__link"><i class="fas fa-list-alt"></i> Latest</router-link>
   </aside>
 </template>
 
