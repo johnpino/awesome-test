328ab4c14505a0211610c7159ee3ab1fd868a2d4
John Pino <iam@johnpino.me>
2020-02-25 01:23:37 -0500

    Improve routing and UX.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <aside class="sidebar">
-    <router-link to="/" class="sidebar__link"><i class="fas fa-home"></i> Home</router-link>
+    <router-link to="/home" class="sidebar__link"><i class="fas fa-home"></i> Home</router-link>
     <router-link to="/top-10" class="sidebar__link"><i class="fas fa-star"></i> Top 10</router-link>
     <router-link to="/latest" class="sidebar__link"><i class="fas fa-list-alt"></i> Latest</router-link>
   </aside>
