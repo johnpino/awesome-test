665baa3b56be1dcb09baa44815766b7c55f7203e
John Pino <iam@johnpino.me>
2020-02-24 22:50:56 -0500

    Add logic to display Top 10 movies using fetch

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <p>Ranking {{ ranking }}</p>
+    <p>Title {{ title }}</p>
+    <p>Release Date {{ release }}</p>
+    <p>Description {{ description }}</p>
+    <img
+      :src="'https://image.tmdb.org/t/p/w500/' + image"
+      alt=""
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieCard',
+  props: {
+    title: String,
+    ranking: Number,
+    description: String,
+    release: String,
+    image: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Top10.vue
+++ src/views/Top10.vue
@@ -1,9 +1,51 @@
 <template>
-  <p>Top 10</p>
+  <div>
+    <h1>Top 10</h1>
+    <MovieCard
+      v-for="movie in movies"
+      :key="movie.title"
+      :title="movie.title"
+      :ranking="movie.ranking"
+      :release="movie.release"
+      :description="movie.description"
+      :image="movie.image"
+    />
+  </div>
 </template>
 
 <script>
+import MovieCard from '../components/MovieCard'
 export default {
-  name: 'Top10'
+  name: 'Top10',
+  components: { MovieCard },
+  data () {
+    return {
+      movies: []
+    }
+  },
+  mounted () {
+    var _this = this
+    fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=ba7fe308f26890a184e00c0917bab2ee&language=en-US&page=1')
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        _this.populateCards(data)
+      })
+  },
+  methods: {
+    populateCards (data) {
+      for (let i = 0; i < 10; i++) {
+        const info = data.results[i]
+        this.movies.push({
+          ranking: info.vote_average,
+          title: info.title,
+          release: info.release_date,
+          description: info.overview,
+          image: info.backdrop_path
+        })
+      }
+    }
+  }
 }
 </script>
