5a223a39f4c601cd738d60670f780b151e372625
John Pino <iam@johnpino.me>
2020-02-25 01:17:44 -0500

    Add logic for latest movies.

--- src/views/Latest.vue
+++ src/views/Latest.vue
@@ -1,9 +1,50 @@
 <template>
-  <p>Latest</p>
+  <section class="content content--top10">
+    <MovieCard
+      v-for="movie in movies"
+      :key="movie.title"
+      :title="movie.title"
+      :ranking="movie.ranking"
+      :release="movie.release"
+      :description="movie.description"
+      :image="movie.image"
+    />
+  </section>
 </template>
 
 <script>
+import MovieCard from '../components/MovieCard'
 export default {
-  name: 'Latest'
+  name: 'Latest',
+  components: { MovieCard },
+  data () {
+    return {
+      movies: []
+    }
+  },
+  mounted () {
+    var _this = this
+    fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=ba7fe308f26890a184e00c0917bab2ee&language=en-US&page=1')
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        _this.populateCards(data)
+      })
+  },
+  methods: {
+    populateCards (data) {
+      for (let i = 0; i < 10; i++) {
+        const info = data.results[i]
+        this.movies.push({
+          ranking: info.vote_average,
+          title: info.title,
+          release: info.release_date,
+          description: info.overview,
+          image: info.backdrop_path
+        })
+      }
+    }
+  }
 }
 </script>
